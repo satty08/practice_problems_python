10259898b922c29199147d8702d0b1ec41e69a31
Satyam <satyambhardawaj@gmail.com>
2020-11-18 20:00:18 +0530

    day5

---
+++ Day5.py
@@ -0,0 +1,12 @@
+'''
+Reverse an array or string
+'''
+
+def reverse(arr):
+    return arr[::-1]
+
+arr = [3, 52, 6, 12, 67, 23, 90]
+print(reverse(arr))
+
+string = 'satyam'
+print(reverse(string))
--- practice.py
+++ practice.py
@@ -3,10 +3,21 @@
 #         print(' '*(n-i-1)+'*'*(2*i+1))
 # star(9)
 
-def fib(n):
-    if n == 0:
-        print(0)
-    if n == 1:
-        print(1)
-    return fib(n-1) + fib(n-2)
-fib(4)
+def main(l):
+    l1 = []
+    l2 = []
+    for i in range(len(l)):
+        if i < len(l)//2:
+            l1.append(l[i])
+        else:
+            l2.append(l[i])
+    l1.remove(min(l1))
+    l2.remove(min(l2))
+    l2 = l2[::-1]
+    sum = 0
+    for i in range(len(l1)):
+        sum += l1[i]*l2[i]
+    return sum
+
+l = [1, 9, 2, 3, 0, 6, 7, 8]
+print(main(l))
