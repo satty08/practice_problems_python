d766a506c2f6d68f4b9273589276c44dce3d7399
Satyam <satyambhardawaj@gmail.com>
2020-11-25 19:57:52 +0530

    day12

--- Day12.py
+++ Day12.py
@@ -37,28 +37,28 @@ Constraints
 1 ≤ rank ≤ 5000
 50 ≤ severity ≤ 1000
 '''
-# t = int(input())
-# while t:
-#     n, origin = input().split()
-#     n = int(n)
-#     laddus = 0
-#     for i in range(n):
-#         temp = input().split()
-#         for j in range(len(temp)):
-#             if temp[j] == 'CONTEST_WON':
-#                 bonus = 20 - int(temp[j+1])
-#                 laddus += 300 + bonus
-#             elif temp[j] == 'TOP_CONTRIBUTOR':
-#                 laddus += 300
-#             elif temp[j] == 'BUG_FOUND':
-#                 laddus += int(temp[j+1])
-#             elif temp[j] == 'CONTEST_HOSTED':
-#                 laddus += 50
-#     if origin == 'INDIAN':
-#         print(laddus//200)
-#     else:
-#         print(laddus//400)
-#     t -= 1
+t = int(input())
+while t:
+    n, origin = input().split()
+    n = int(n)
+    laddus = 0
+    for i in range(n):
+        temp = input().split()
+        for j in range(len(temp)):
+            if temp[j] == 'CONTEST_WON':
+                bonus = 20 - int(temp[j+1])
+                laddus += 300 + bonus
+            elif temp[j] == 'TOP_CONTRIBUTOR':
+                laddus += 300
+            elif temp[j] == 'BUG_FOUND':
+                laddus += int(temp[j+1])
+            elif temp[j] == 'CONTEST_HOSTED':
+                laddus += 50
+    if origin == 'INDIAN':
+        print(laddus//200)
+    else:
+        print(laddus//400)
+    t -= 1
 
 '''
 Write a method to replace all spaces in a string with '%20: You may assume that the string
