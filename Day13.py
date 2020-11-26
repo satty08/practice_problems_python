b6fb71dbe0e3eeed1a8b86a48505dfd7c66a19b7
Satyam <satyambhardawaj@gmail.com>
2020-11-26 19:48:46 +0530

    day13

--- Day13.py
+++ Day13.py
@@ -50,3 +50,22 @@ def inversion(arr, n):
     return res
 arr = [2, 4, 1, 3, 5]
 print(inversion(arr, len(arr)))
+
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an 
+algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+'''
+
+def maxProfit(prices):
+    minprice = 10**9
+    maxprofit = 0
+    for i in range(len(prices)):
+        if prices[i] < minprice:
+            minprice = prices[i]
+        elif prices[i] - minprice > maxprofit:
+            maxprofit = prices[i] - minprice
+    return maxprofit
