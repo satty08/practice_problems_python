c999544a61f183a47cc724331892f52eaac9da36
Satyam <satyambhardawaj@gmail.com>
2020-12-02 17:28:05 +0530

    ninja 2.0 practice

--- practice.py
+++ practice.py
@@ -1,13 +1,27 @@
-def checksum(a, b):   
-    res = 0
-    for i in range(a, b+1):
-        for j in range(i, b+1):
-            if (i+j)%8 == 0 or (i+j)%14 == 0:
-                if i == j:
-                    res += 1
-                else:
-                    res += 2
-                print(i, j)
-    return res
+# a = int(input())
+# n = int(input())
+# x = list(map(int, input().split()))
+# b = 0
+# i = 0
+# res = 1
+# while n > 0:
+#     power = 10**(n-1)
+#     b = res*(a**(x[i]*power))
+#     res = b%1337
+#     n -= 1
+#     i += 1
+# print(res)
 
-print(checksum(4, 8))
+t = int(input())
+while t:
+
+    n = int(input())
+    strength = list(map(int, input().split()))
+    count = 0
+    for i in range(n):
+        for j in range(i, n):
+            if strength[i] > 2*strength[j]:
+                count += 1
+    print(count)
+    t -= 1
+# 1 3 2 6 2 7 4 2 3 1
