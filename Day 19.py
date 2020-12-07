3419c7010ca1702872044c9dfd3f318258b9fb31
Satyam <satyambhardawaj@gmail.com>
2020-12-07 13:20:21 +0530

    day19

---
+++ Day 19.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root):
+        node = root
+        while node:
+            curr = dummy = Node(0)
+            while node:
+                if node.left:
+                    curr.next = node.left
+                    curr = curr.next
+                if node.right:
+                    curr.next = node.right
+                    curr = curr.next
+                node = node.next
+            node = dummy.next
+        return root
