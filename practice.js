da0dccd6ed6bf68cc25619d8e4975c65471601df
Satyam <satyambhardawaj@gmail.com>
2021-01-31 17:30:56 +0530

    js practice

--- practice.js
+++ practice.js
@@ -1,10 +1,20 @@
-const foo = () => {
-    let a = b = 10
-    a++;
-    return a;
-}
+// const foo = () => {
+//     let a = b = 10
+//     a++;
+//     return a;
+// }
 
-foo()
+// foo()
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+const arr1 = ['a', 'b', 'c']
+const arr2 = ['b', 'c', 'a']
+console.log(
+    arr1.sort() === arr1,
+    arr2.sort() == arr2,
+    arr1.sort() === arr2.sort(),
+    arr1.sort(),
+    arr2.sort()
+);
