b856145ecf02a54de72fafbc7116ea36a1fc5309
Satyam <satyambhardawaj@gmail.com>
2021-02-08 11:07:56 +0530

    day44

--- practice.py
+++ practice.py
@@ -202,27 +202,99 @@ x = bin(n).replace('0b', '')
 print(type(x))
 
 
-n = int(input())
+# t = int(input())
+# while t:
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#     arr.sort()
+#     sum_t = abs(arr[0]-arr[1])+ abs(arr[0]-arr[n-1])+ abs(arr[n-1]-arr[1])
+#     print(sum_t)
+#     t -= 1
 
-amount = list(map(int, input().split()))
+# s = 'aaab'
+# res = []
+# count = 0
+# for i in s:
+#     if i == 'b':
+#         res.append(count)
+#     count += 1
+# ans = []
+# for i in range(len(s)):
+#     diff = 10**9
+#     for j in res:
+#         diff = min(diff, abs(i-j))
+#     ans.append(diff)
 
-p = int(input())
-pairs = []
-for i in range(p):
-    arr = list(map(int, input().split()))
-    pairs.append(arr)
+# print(ans)
 
-a = []
-res = []
-a.append(pairs[0][0])
-a.append(pairs[0][1])
-for i in range(1, p):
-    if pairs[i][0] in a or pairs[i][0] in a:
-        a.append(pairs[i][0])
-        a.append(pairs[i][1])
 
-    elif pairs[i][0] not in a and pairs[i][0] not in a:
-        res.append(a)
-        a = []
-        a.append(pairs[i][0])
-        a.append(pairs[i][1])
+p = list(map(str, input().split()))                                      #p = 12:01 AM
+n = int(input())                                                  #n = 4
+time = []                                                            
+for i in range(n):                                         # [12:00 AM 11:42 PM, 12:01 AM 11:59 AM, 12:30 AM 12:00 PM, 11:59 AM 11:59 PM]
+    x = list(map(str, input().split()))                    # res = 1100
+    time.append(x)
+res = ''
+# p converted to 2400 HRS
+t = 0
+pres = list(map(int, p[0].split(':')))
+if p[1] == 'AM':
+    if pres[0] == 12:
+        t = pres[1]
+    else:
+        t = pres[0]*100 + pres[1]
+
+elif p[1] == 'PM':
+    if pres[0] == 12:
+        t = pres[0]*100 + pres[1]
+    else:
+        t = 1200 + pres[0]*100 + pres[1]
+print(t)
+for i in range(n):                                    # time[i] = ['12:00', 'AM', '11:42', 'PM']
+    ans = []
+    for j in range(0, 4, 2):
+        curr = list(map(int, time[i][j].split(':')))
+        if time[i][j+1] == 'AM':
+            if curr[0] == 12:
+                a = curr[1]
+            else:
+                a = curr[0]*100 + curr[1]
+
+        if time[i][j+1] == 'PM':
+            if curr[0] == 12:
+                a = curr[0]*100 + curr[1]
+            else:
+                a = 1200 + curr[0]*100 + curr[1]
+
+        ans.append(a)
+    print(ans)
+    if t >= ans[0] and t <= ans[1]:
+        res += '1'
+    else:
+        res += '0'
+
+print(res)
+        
+
+
+
+
+
+
+# time = list(map(str, input().split()))
+# print(time)
+# if time[1] == 'AM':
+#     res = list(map(int, time[0].split(':')))
+#     if res[0] == 12:
+#         t = res[1]
+#     else:
+#         t = res[0]*100 + res[1]
+
+# elif time[1] == 'PM':
+#     res = list(map(int, time[0].split(':')))
+#     if res[0] == 12:
+#         t = res[0]*100 + res[1]
+#     else:
+#         t = 1200 + res[0]*100 + res[1]
+
+# print(t)
