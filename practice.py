c59675698239eee8634c6132e8bfd88948652736
Satyam <satyambhardawaj@gmail.com>
2021-02-21 18:50:38 +0530

    day48

--- practice.py
+++ practice.py
@@ -303,3 +303,22 @@ print(type(x))
     
 # print(letterCase(s))
 
+def romanToInt(s):
+    d = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    out = 0
+    for i in range(len(s)-1):
+        print(d[s[i]])
+        if d[s[i]]<d[s[i+1]]:
+            out -= d[s[i]]
+        else:
+            out += d[s[i]]
+    out+= d[s[-1]]
+    return out
