7ae43f8b65e756f2b7f7bcf4e09dac16bc5d1f5d
Satyam <satyambhardawaj@gmail.com>
2021-04-18 22:00:45 +0530

    day75

--- Day75.py
+++ Day75.py
@@ -1,15 +1,15 @@
-mat = [
-    [0,1,0],
-    [1,1,1],
-    [0,1,0]
-]
-t = 0
-res = 0
-x, y = len(mat), len(mat[0])
-for i in range(x):
-    for j in range(y):
-        print(sum(mat[0:i][0:j]), i)
-print(res)
+# mat = [
+#     [0,1,0],
+#     [1,1,1],
+#     [0,1,0]
+# ]
+# t = 0
+# res = 0
+# x, y = len(mat), len(mat[0])
+# for i in range(x):
+#     for j in range(y):
+#         print(sum(mat[0:i][0:j]), i)
+# print(res)
 
 '''
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
@@ -37,3 +37,20 @@ class Solution:
             temp = temp.next
             
         return root.next
+
+    
+details = [
+    ['Harry Kane', 'Tottenham', 19, 12],
+    ['Mohd. Salah', 'Liverpol', 19, 11],
+    ['Bruno Fernandes', 'Man Utd', 16, 12],
+    ['Patrick Bamford', 'Leeds Utd', 14, 13],
+    ['Jamie Vardy', 'Leicester', 12, 13]
+]
+
+avg = {}
+
+for i in range(len(details)):
+    x = round(details[i][2]/details[i][3], 3)
+    avg[details[i][0]] = x
+
+print(avg)
