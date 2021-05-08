7022f518082329a2e9b48597202ba25441302053
Satyam <satyambhardawaj@gmail.com>
2021-05-08 08:25:21 +0530

    practice

--- Day88.py
+++ Day88.py
@@ -33,19 +33,57 @@ class Solution:
         
         return BST(0, len(res)-1, res, None)
 
-s = 'caba'
-i, j = 0, len(s)
-ans = ''
-res = 0
-flag = 0
-while i < j:
-    if s[i:j] == s[i:j][::-1]:
-        res = max(res, j-i)
-        ans = s[i:j]
-    if flag == 0:
-        i += 1
-        flag = not flag
-    else:
-        j -= 1
-    print(ans)
-print(ans)
+# s = 'caba'
+# i, j = 0, len(s)
+# ans = ''
+# res = 0
+# flag = 0
+# while i < j:
+#     if s[i:j] == s[i:j][::-1]:
+#         res = max(res, j-i)
+#         ans = s[i:j]
+#     if flag == 0:
+#         i += 1
+#         flag = not flag
+#     else:
+#         j -= 1
+#     print(ans)
+# print(ans)
+# count = 0
+# for i in range((2**50)-1):
+#     if i^(i+1) == (i+2)^(i+3):
+#         count += 1
+
+# print(count)
+
+print(9>>1)
+
+# def solve(i, j, x):
+#     res = 1
+#     i = i%x
+#     if i == 0:
+#         return 0
+        
+#     while j > 0:
+#         if (j&1):
+#             res = (res*i)%x
+#         j = j>>1
+#         i = (i*i)%x
+        
+#     return res
+
+# try:
+#     mod = 10**9+7
+#     t = int(input())
+#     while t:
+#         n = int(input())
+#         print(solve(2, n-1, mod))
+# except:
+#     pass
+
+
+board = ['XOX', 'XXO', 'XOO']
+if board[0][0] == board[0][1] == board[0][2]:
+    print(True)
+else:
+    print(False)
